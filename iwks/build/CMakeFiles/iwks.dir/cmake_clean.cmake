0beba66da13476c9727b9e807f3701dcccf261c9
apoorvamalemath <35571354+apoorvamalemath@users.noreply.github.com>
2021-11-17 13:43:10 -0500

    Add files via upload

---
+++ iwks/build/CMakeFiles/iwks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/iwks.dir/iwks.cpp.o"
+  "iwks.pdb"
+  "iwks"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/iwks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
